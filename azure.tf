f66f80870d9cf86c41de8b3f099facdfc63f4271
hennie <henniefrancis@gmail.com>
2023-11-28 20:34:52 +0200

    Re-Create Resources

--- azure.tf
+++ azure.tf
@@ -8,23 +8,23 @@
 # See https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 ##########################################################################################################################
 
-# resource "azurerm_resource_group" "dotnet-conf-2023-azure-rg" {
-#   provider = azurerm
-#   name     = "dotnet-conf-2023"
-#   location = var.azure_region.afw
-#   tags     = var.default_tags
-# }
+resource "azurerm_resource_group" "dotnet-conf-2023-azure-rg" {
+  provider = azurerm
+  name     = "dotnet-conf-2023"
+  location = var.azure_region.afw
+  tags     = var.default_tags
+}
 
-# resource "azurerm_storage_account" "dotnet-conf-2023-azure-sa" {
-#   name                     = "dotnetconf2023storageacc"
-#   resource_group_name      = azurerm_resource_group.dotnet-conf-2023-azure-rg.name
-#   location                 = azurerm_resource_group.dotnet-conf-2023-azure-rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-#   tags = merge(var.default_tags,
-#     {
-#       cloudprovider = "Azure"
-#       event         = ".NET Conference 2023"
-#       location      = "Cape Town"
-#   })
-# }
+resource "azurerm_storage_account" "dotnet-conf-2023-azure-sa" {
+  name                     = "dotnetconf2023storageacc"
+  resource_group_name      = azurerm_resource_group.dotnet-conf-2023-azure-rg.name
+  location                 = azurerm_resource_group.dotnet-conf-2023-azure-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags = merge(var.default_tags,
+    {
+      cloudprovider = "Azure"
+      event         = ".NET Conference 2023"
+      location      = "Cape Town"
+  })
+}
